6326f9db12b894c6cefe3af6fbec134f7d91a790
Nitolapio <alexandrunitu3e@gmail.com>
2020-10-16 12:58:08 +0200

    Revert "Initialized window values. Errors"
    This reverts commit dca2644591c701f346a331cb0f46d8d9405f3ec5.

--- core/src/com/gdx/dogs_and_dungeons/DogsAndDungeons.java
+++ core/src/com/gdx/dogs_and_dungeons/DogsAndDungeons.java
@@ -1,8 +1,5 @@
 package com.gdx.dogs_and_dungeons;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gdx.dogs_and_dungeons.screens.MainGameScreen;
 
 // Clase para gestionar las distintas ventanas del juego gracias a la herencia de Game
@@ -12,25 +9,17 @@ public class DogsAndDungeons extends Game {
 
 	public static final MainGameScreen mainGameScreen = new MainGameScreen();
 
-	public OrthographicCamera camera;   // Ortographic 2D camera. lookAt(float x, float y, float z) for scroll on player position
-	public SpriteBatch spriteBatch;    // We're gonna use this to draw sprites http://rbwhitaker.wikidot.com/spritebatch-basics
-	public BitmapFont bitmapFont;   //Bitmap font with sprites https://stackoverflow.com/questions/12895822/how-to-draw-a-bitmapfont-in-libgdx
-
 	@Override
 	public void create() {
-		setScreen(mainGameScreen); // We set the main screen
 
-		// Creation of declared variables
-		spriteBatch = new SpriteBatch();
-		bitmapFont = new BitmapFont();
+		setScreen(mainGameScreen);
 
 	}
 
 	@Override
 	public void dispose() {
-		// All the variables go here when disposed
+
 		getScreen().dispose();
-		spriteBatch.dispose();
-		bitmapFont.dispose();
+
 	}
 }
--- core/src/com/gdx/dogs_and_dungeons/screens/MainGameScreen.java
+++ core/src/com/gdx/dogs_and_dungeons/screens/MainGameScreen.java
@@ -1,23 +1,12 @@
 package com.gdx.dogs_and_dungeons.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.gdx.dogs_and_dungeons.DogsAndDungeons;
 
 // Pantalla de juego
 
 public class MainGameScreen implements Screen {
 
-    //final DogsAndDungeons game; // Constant variable of the Game class
-
-    public OrthographicCamera camera;
-
     public MainGameScreen () {
-
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1024, 768); //yDown false: normal graphic coordinates
 
 
     }
