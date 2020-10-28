f26d3249d74153e8ad5ee9bc43ffff12adf4bfd6
Nitolapio <alexandrunitu3e@gmail.com>
2020-10-28 11:15:46 +0100

    background images added + background change

--- core/src/com/gdx/dogs_and_dungeons/screens/OptionsScreen.java
+++ core/src/com/gdx/dogs_and_dungeons/screens/OptionsScreen.java
@@ -3,6 +3,9 @@ package com.gdx.dogs_and_dungeons.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -34,46 +37,38 @@ public class OptionsScreen implements Screen {
 
     private DogsAndDungeons game_ref;
 
+    private Texture backgroundTexture;
+
+    private SpriteBatch batch;
+
+
     public OptionsScreen(DogsAndDungeons game) {
 
         game_ref = game;
-
         stage = new Stage();
 
+
         // La tabla es un layout muy flexible que permite colocar actores
-
         table = new Table();
-
         // table.setDebug(true); // Mostramos cómo se posicionan los elemnetos en la tabla
-
         // Hacemos que la tabla ocupe toda la pantalla
-
         table.setFillParent(true);
 
         Label.LabelStyle labelStyle = new Label.LabelStyle();
-
         labelStyle.font = Utility.mainFont;
-
         label = new Label("Opciones",labelStyle);
-
         volumeLabel = new Label("Volumen", labelStyle);
-
         musicLabel = new Label("Música",labelStyle);
 
         // Cargamos la skin, que contine texturas para todos los widgets (elementos UI)
-
         Slider slider = new Slider(0,10,1,false, Utility.DEFAULT_SKIN);
-
         musicBox = new CheckBox("",Utility.DEFAULT_SKIN);
 
         fpsLabel = new Label("FPS",labelStyle);
-
         fpsLimiter = new SelectBox<Integer>(Utility.DEFAULT_SKIN);
-
         fpsLimiter.setItems(60,45,30);
 
         backButton = new TextButton("Volver",Utility.DEFAULT_SKIN);
-
         backButton.addListener(new ClickListener() {
 
 
@@ -90,35 +85,29 @@ public class OptionsScreen implements Screen {
 
         applyButton = new TextButton("Aplicar cambios",Utility.DEFAULT_SKIN);
 
+
+        // Background batch and texture
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture(Gdx.files.internal("backgrounds/Background8.png"));
+
+        // Adding components to table
         table.top();
-
         table.add(label).padTop(20).colspan(2);
-
         table.row().expandY();
-
         table.add(volumeLabel).expandX().right().padRight(20);
-
         table.add(slider).expandX().left().padLeft(20);
-
         table.row().expandY();
-
         table.add(musicLabel).right().padRight(20);
-
         table.add(musicBox).left().padLeft(20);
-
         table.row().expandY();
-
         table.add(fpsLabel).right().padRight(20);
-
         table.add(fpsLimiter).left().padLeft(20);
-
         table.row().expandY();
-
         table.add(backButton);
-
         table.add(applyButton);
-
         stage.addActor(table);
+
+
 
 
 
@@ -137,6 +126,11 @@ public class OptionsScreen implements Screen {
     public void render(float delta) {
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Background
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, 800, 500);
+        batch.end();
 
         stage.act(delta); // Llama al método act() de cada actor
 
