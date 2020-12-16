9a89a965684c58919f5ce8146ab47af92d339d11
Jon Andoni Castillo Ciruela <Andowna9@users.noreply.github.com>
2020-12-16 15:35:07 +0100

    UsersScreen y diálogo para introducir contraseñas mejorados

--- core/src/main/com/gdx/dogs_and_dungeons/screens/UsersScreen.java
+++ core/src/main/com/gdx/dogs_and_dungeons/screens/UsersScreen.java
@@ -1,14 +1,22 @@
 package com.gdx.dogs_and_dungeons.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFontCache;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.gdx.dogs_and_dungeons.DogsAndDungeons;
 import com.gdx.dogs_and_dungeons.Utility;
@@ -35,22 +43,46 @@ public class UsersScreen implements Screen {
 
     private Array<User> userModel;
 
-    private PasswordDialog dPass;
 
     // Diálogo para introducir clave/contraseña
 
-    class PasswordDialog extends Dialog {
+    private class PasswordDialog extends Dialog {
 
         private TextField tPass;
 
+        private Label outputMessage;
 
-        public PasswordDialog(String title, Skin skin) {
+        private User user;
+
+        private void updateSize() {
+
+            getContentTable().getCell(outputMessage).height(outputMessage.getPrefHeight());
+
+
+            // Actualizamos el tamaño del diálogo tras insertar el nuevo elemento (label)
+
+            pack();
+
+            // Se centra el diálogo
+
+            setPosition(Math.round((stage.getWidth() - getWidth()) / 2), Math.round((stage.getHeight() - getHeight()) / 2));
+
+        }
+
+
+        public PasswordDialog(String title, Skin skin, User user) {
 
             super(title, skin);
 
+            this.user = user;
+
             // Label para indicar que se espera la contraseña
 
-            Label lPass = new Label("Introduce la clave: ", Utility.DEFAULT_SKIN);
+            Label lPass = new Label(String.format("Introduce la clave para %s: ",user.getNickname()), Utility.DEFAULT_SKIN);
+
+            // Mensaje de error o éxito después de comprobar contraseña
+
+            outputMessage = new Label("",Utility.DEFAULT_SKIN);
 
             // Campo de texto oculto para que no se visualice la contraseña
 
@@ -75,8 +107,32 @@ public class UsersScreen implements Screen {
 
                     Gdx.app.debug(TAG, "Contraseña introducida: " + password);
 
-                    hide();
+                    if (password.isEmpty()) {
 
+                        // Se borra el anterior mensaje y si no existía se añade una fila también
+
+                        // Se añade de nuevo
+
+                        outputMessage.setText("Clave incorrecta!");
+
+                        outputMessage.setColor(Color.RED);
+
+
+                    }
+
+                    else {
+
+                        outputMessage.setText("Clave correcta!");
+
+                        outputMessage.setColor(Color.GREEN);
+
+                        updateSize();
+
+                        hide(Actions.fadeOut(1.5f));
+
+                    }
+
+                    updateSize();
 
                 }
 
@@ -103,19 +159,20 @@ public class UsersScreen implements Screen {
 
             getButtonTable().add(cancelButton,confirmButton);
 
+            getContentTable().row();
+
+            getContentTable().add(outputMessage).height(0);
 
         }
+
     }
+
 
     public UsersScreen(DogsAndDungeons game){
 
         this.game_ref = game;
 
         userModel = new Array<>();
-
-        // Diálogo para contraseña
-
-        dPass = new PasswordDialog("Advertencia!", Utility.DEFAULT_SKIN);
 
         gradient = new ShapeRenderer();
 
@@ -125,59 +182,64 @@ public class UsersScreen implements Screen {
 
         userList = new List<>(Utility.DEFAULT_SKIN);
 
-        addUser(new User("Asier", "Jauregui"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
-        addUser(new User("Asier", "Josejuan Alberto"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
-        addUser(new User("Asier", "Jauregui"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
-        addUser(new User("Asier", "Jauregui"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
-        addUser(new User("MMMMMeEEEEGGGGGGAAAAAMMMMIIIINNNNDDDDd", "Jauregui"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
-        addUser(new User("Asier", "Jauregui"));
-        addUser(new User("Jon Andoni", "Castillo"));
-        addUser(new User("Alex", "Nitu"));
+        userList.addListener(new ActorGestureListener() {
 
-        bCreate = new TextButton("Crear usuario", Utility.DEFAULT_SKIN);
-
-        bCreate.addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
+            public void tap (InputEvent event, float x, float y, int count, int button) {
 
-                dPass.show(stage);
+                if (count == 2) {
+
+                    User selectedUser = userList.getSelected();
+
+                    Gdx.app.log(TAG, "Usuario seleccionado: " + selectedUser);
+
+                    // Mostramos un nuevo diálogo para introducir contraseña
+
+                    PasswordDialog dPass = new PasswordDialog("Advertencia!", Utility.DEFAULT_SKIN,selectedUser);
+
+                    dPass.show(stage);
+
+                }
 
             }
         });
 
+        addUser(new User("Asier", "añlsasal"));
+        addUser(new User("Jon Andoni", "sañlkñsa"));
+        addUser(new User("Alex", "alklas"));
+        addUser(new User("Alexby11","LKAJ"));
+        addUser(new User("Willyrex","alñjsksa"));
+        addUser(new User("Vegeta777","lakKJASK"));
+        addUser(new User("ElRubius","añksldsadñ"));
+
+        bCreate = new TextButton("Crear usuario", Utility.DEFAULT_SKIN);
+
         bDelete = new TextButton("Borrar usuario", Utility.DEFAULT_SKIN);
 
         Label.LabelStyle style = new Label.LabelStyle();
+
         style.font = Utility.mainFont;
 
         lSelectUser = new Label("Selecciona un usuario:", style);
 
-        ScrollPane scroller = new ScrollPane(userList);
+        ScrollPane scroller = new ScrollPane(userList, Utility.DEFAULT_SKIN);
 
         Table table = new Table();
 
-        //table.setDebug(true); Quitar comentario para ver la alineación de la tabla
+        //table.setDebug(true); //Quitar comentario para ver la alineación de la tabla
 
 
         table.setFillParent(true);
 
+        table.center();
+
         table.top();
-        table.add(lSelectUser).padTop(50).left().padLeft(50);
-        table.row().expandY();
-        table.add(scroller).padLeft(100).padBottom(50).padTop(50);
-        table.row();
-        table.add(bCreate).padRight(50).padBottom(100);
-        table.add(bDelete).padRight(150).padBottom(100);
+        table.add(lSelectUser).padTop(25).colspan(2); //padTop(50).left().padLeft(50);
+        table.row().expandY().space(20);
+        table.add(scroller).colspan(2).width(500); //padLeft(100).padBottom(50).padTop(50);
+        table.row().expandY().space(20);
+        table.add(bCreate).right().padBottom(25); //padRight(50).padBottom(100);
+        table.add(bDelete).left().padBottom(25);  //padRight(150).padBottom(100);
 
 
         stage.addActor(table);
@@ -190,6 +252,7 @@ public class UsersScreen implements Screen {
     }
     @Override
     public void show() {
+
         Gdx.input.setInputProcessor(stage);
     }
 
