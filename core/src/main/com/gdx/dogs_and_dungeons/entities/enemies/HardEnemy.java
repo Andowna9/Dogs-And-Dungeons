d1cc1acc4582809ebd3f919f627eb4225bd99e12
AsierJauregi <asierjauregi@opendeusto.es>
2020-12-28 16:00:13 +0100

    Añadido cementerio al mapa y comienzo de la implementacion de hard enemy

--- core/src/main/com/gdx/dogs_and_dungeons/entities/enemies/HardEnemy.java
+++ core/src/main/com/gdx/dogs_and_dungeons/entities/enemies/HardEnemy.java
@@ -1,16 +1,41 @@
 package com.gdx.dogs_and_dungeons.entities.enemies;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class HardEnemy extends Enemy {
 
-    private static final String specificPath = "";
+    private static final String specificPath = "ghost.png";
+    private static final String TAG = HardEnemy.class.getSimpleName();
+    private Rectangle zonOfVision = new Rectangle(getCurrentPosition().x, getCurrentPosition().y, 30,30);
+    private boolean isFollowing = false;
 
     public HardEnemy(int width, int height,float drawWidth, float drawHeight) {
 
         super(width, height, drawWidth, drawHeight, specificPath);
+
+        setState(State.IDLE);
+
+        setVelocity(2f, 2f);
     }
 
     @Override
     public void behave(float delta) {
 
     }
+    public void followPlayer(float playerX, float playerY){
+        if (zonOfVision.contains(playerX, playerY)){
+            isFollowing = true;
+        }
+    }
+    public void calculateDirection(float playerX, float playerY){
+        /*if(isFollowing){
+            if(getCurrentPosition().x > playerX){
+                setDirection(Direction.LEFT);
+
+          }
+            */
+    }
+
+
+
 }
